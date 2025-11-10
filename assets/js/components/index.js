091ce71d6dbdb0ee8f2963726bff595a4460d2f1
sebousan <sebousan@gmail.com>
2025-11-10 10:58:26 +0100

    improve modal video

--- assets/js/components/index.js
+++ assets/js/components/index.js
@@ -1,3 +1,13 @@
 import './menu';
 // import './toc';
 import './video';
+
+
+const modalElement = document.getElementById('modalVideo');
+const videoModalElement = modalElement.getElementsByTagName('video')[0];
+modalElement.addEventListener('hide.bs.modal', event => {
+  videoModalElement.pause();
+})
+modalElement.addEventListener('shown.bs.modal', event => {
+  videoModalElement.play();
+})
